ccbf4e369349c0d42775714b8ffd4d34422c84bd
Nicolas <nicolaskrusser@gmail.com>
2018-07-22 19:06:43 +0200

    use logo

--- burger-builder/src/components/Navigation/ToolBar/ToolBar.js
+++ burger-builder/src/components/Navigation/ToolBar/ToolBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import classes from './ToolBar.css'
+import classes from './ToolBar.css';
+import Logo from '../../Logo/Logo';
 const toolbar = (props)=>(
     <header className={classes.ToolBar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>
             <ul>
                ....
