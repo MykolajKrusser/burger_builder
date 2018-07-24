9b3a593c57ed25d27f2df2500c90d58d36691b16
Nicolas <nicolaskrusser@gmail.com>
2018-07-24 17:25:10 +0200

    more responsive adjustments

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-const sideDrawer = (props)=>{
 
+const sideDrawer = (props)=>{
     return(
         <div className={classes.SideDrawer}>
             <div className={classes.Logo}>
