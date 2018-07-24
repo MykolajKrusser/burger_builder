9a983337028a70ab06b7e8514a0ab34e25a1a43b
Nicolas <nicolaskrusser@gmail.com>
2018-07-24 16:49:47 +0200

    responsive logo

--- burger-builder/src/components/Logo/Logo.js
+++ burger-builder/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './Logo.css';
 import burgerLogo from '../../assets/img/burger-logo.png';
 const logo = (props)=>(
     <div className={classes.Logo}>
-        <img src={burgerLogo} alt="Burger Logo"/>
+        <img src={burgerLogo} style={{height: props.height}} alt="Burger Logo"/>
     </div>
 );
 export default logo;
