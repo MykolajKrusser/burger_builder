ecb01652cdf8f576bdc483f5e8bd9973296f3b29
Nicolas <nicolaskrusser@gmail.com>
2018-07-28 13:16:34 +0200

    added style to menu button

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawerToggle/SideDrawerToggle.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawerToggle/SideDrawerToggle.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import classes from './SideDrawerToggle.css';
+
 const sideDrawerToggle = (props)=>(
-    <div onClick={props.clicked}>MENU</div>
+    <div onClick={props.clicked} className={classes.DrawerToggle}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 export default sideDrawerToggle;
