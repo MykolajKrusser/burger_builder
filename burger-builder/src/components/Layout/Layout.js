89241a92deb28f04889bb13ef30e25e84af5a2b4
Nicolas <nicolaskrusser@gmail.com>
2018-07-28 12:27:10 +0200

    reuse backDrop component

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Wrap from '../../hoc/Wrap';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/ToolBar/ToolBar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    <Wrap>
-        <SideDrawer/>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Wrap>
-);
-export default layout;
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerCloseHandler = ()=>{
+        this.setState({showSideDrawer: false})
+    }
+    render(){
+        return (
+         <Wrap>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+            <Toolbar/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Wrap>
+        );
+    }
+}
+export default Layout;
