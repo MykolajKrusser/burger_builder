dbab6149bb37f0a42575c174de68017dc2c83f1c
Nicolas <nicolaskrusser@gmail.com>
2018-07-29 12:29:29 +0200

    added component render when needed

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Wrap from '../../../hoc/Wrap';
 import Button from '../../Ui/Button/Button';
 
-const orderSammary = (props)=>{
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSammary extends Component {
+    // this component cold be a fuctional, does not have be a class
+    componentWillUpdate(){
+        console.log("WillUPdete component odred summary!!")
+    }
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey =>{
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{textTransform: "capitalize"}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>
             );
         })
-    return(
-        <Wrap>
-            <h3>Your order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Price: <strong>{props.price.toFixed(2)}</strong>$</p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchasCancel}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchasContinue}>Continue</Button>
-        </Wrap>
-    );
+        return(
+            <Wrap>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Price: <strong>{this.props.price.toFixed(2)}</strong>$</p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchasCancel}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchasContinue}>Continue</Button>
+            </Wrap>
+        );
+    }
+    
     
 };
 
-export default orderSammary;
+export default OrderSammary;
--- burger-builder/src/components/Ui/Modal/Modal.js
+++ burger-builder/src/components/Ui/Modal/Modal.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Wrap from '../../../hoc/Wrap';
 import Backdrop from '../BackDrop/Backdrop';
 
-const modal = (props)=>(
-    <Wrap>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Wrap>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
+    componentWillUpdate(){
+        console.log("Modal will update!!")
+    }
+    render(){
+        return (
+            <Wrap>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Wrap>
+        );
+    }
+    
+}
 
-export default modal;
+export default Modal;
