f0d0fe7988cd271f12c2d97d7852921dfde006e5
Nicolas <nicolaskrusser@gmail.com>
2018-09-02 14:22:57 +0200

    adding custom form validation

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },      
             street: {
                 elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipcode: {
                 elementType: 'input',
@@ -30,7 +38,13 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength:5,
+                    maxLength:5
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -38,7 +52,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -46,7 +64,11 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'Your E-mail'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -60,6 +82,21 @@ class ContactData extends Component{
             }
         },
         loading: false
+    }
+    
+    checkValidity(value, rules){
+        let isValid = false;
+        if (rules.required){
+            isValid = value.trim() !== '';
+        }
+        if (rules.minLength){
+            isValid = value.length >= rules.minLength
+        }
+        if (rules.minLength){
+            isValid = value.length <= rules.maxLength
+        }
+
+        return isValid
     }
 
     orderHandler = (event)=>{
@@ -94,6 +131,8 @@ class ContactData extends Component{
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        console.log(updatedFormElement);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         this.setState({orderForm: updatedOrderForm})
 
