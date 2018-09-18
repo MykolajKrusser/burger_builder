329e96a45a0f222d89ece05ac1e123c9fc78cfe7
Nicolas <nicolaskrusser@gmail.com>
2018-09-18 11:40:31 +0200

    installing redux and react-redux

---
+++ burger-builder/src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionsType from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalprice: 4
+}
+
+const reducer = (state=initialState, actions)=>{
+
+}
+
+export default reducer;
