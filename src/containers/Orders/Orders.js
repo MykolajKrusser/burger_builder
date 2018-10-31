fa5522a03616dede97dd4abbc2e799a690f311cc
Nicolas <nicolaskrusser@gmail.com>
2018-10-31 12:44:35 +0100

    Fetching Orders (via Redux)

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,54 +1,53 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Order from '../../components/Order/Order';
 import Loader from '../../components/Ui/Loader/Loader';
+import * as actions from '../../store/actions/index';
 
 class Orders extends Component{
-    state={
-        orders: [],
-        loading: true
-    }
+
     componentDidMount(){
-        axios.get('/orders.json')
-            .then(res =>{
-                const fetchedOrders = []
-                for(let key in res.data){
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id: key
-                    })
-                }
-                this.setState({loading: false, orders: fetchedOrders})
-            })
-            .catch(err=>{
-                this.setState({loading: false})
-            })
-    }
+       this.props.onFetchOrders();
+    };
+
     render(){
-        let orders = this.state.orders.map(order=>{
-            return <Order 
-                key={order.id}
-                ingredients={order.ingredients}
-                price={+order.price}/>
-        })
+        let orders=<Loader/>
         
-        let messege = <p style={{textAlign: "center"}}>You have no orders</p>
+        if(!this.props.loading){
+            orders = this.props.orders.map(order=>{
+                return <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={+order.price}/>
+            });
+        }
+        
+        let messege = <p style={{textAlign: "center"}}>You have no orders</p>;
 
-        if (this.state.orders.length === 0){
+        if (this.props.orders.length === 0){
             orders = messege
-        }
-
-        if (this.state.loading){
-            orders=<Loader/>
-        }
+        };
         
         return(
             <div>
                 {orders}
             </div>
         );
-    }
-}
+    };
+};
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state=>{
+    return{
+        orders: state.order.orders,
+        loading: state.order.loading
+    };
+};
+
+const mapDispatchToProps = dispatch=>{
+    return{
+        onFetchOrders: ()=> dispatch(actions.fetchOrders())
+    };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
