f98d2bb93a1e1a6bd2312a4086d4dfba200798e4
Nicolas <nicolaskrusser@gmail.com>
2018-11-03 12:06:30 +0100

    Storing the Token

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,35 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false
+}
+
+const reduser = (state=initState, action)=>{
+    switch(action.type){
+        case actionTypes.AUTH_START:
+        return{
+            ...state,
+            error: null,
+            loading: true
+        }
+        case actionTypes.AUTH_SUCCESS:
+        return{
+            ...state,
+            error: null,
+            loading: false,
+            token: action.idToken,
+            userId: action.userId
+        }
+        case actionTypes.AUTH_FAIL:
+        return{
+            error: action.error,
+            loading: false
+        }
+        default: return state;
+    }
+};
+
+export default reduser;
